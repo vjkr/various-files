1dee99c61b07caa1eab0d897f3960bc06a00869d
vjkr <hi.vjkr@gmail.com>
2016-05-27 15:39:54 +0530

    Add files via upload

---
+++ Opening_exe_html.py
@@ -0,0 +1,11 @@
+import webbrowser
+webbrowser.open('http://gmail.com')
+webbrowser.open('http://github.com')
+webbrowser.open('https://www.youtube.com/watch?v=hnswwRWLi3E')
+import os
+#
+os.startfile("C:\Program Files\Kotato\FLV Downloader\FLVDownloader.exe")
+#os.startfile("D:\killme\basi_v\basi_v.xise")
+os.startfile("C:\Program Files\Kotato\FLV Downloader\FLVDownloader.exe")
+#os.startfile("C:\Users\Vijaykumar\Desktop\ML_vjkr.pptx")
+
